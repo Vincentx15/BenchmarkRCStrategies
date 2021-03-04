e4332edbc0b6761840ceed90d4bbc058833eb73e
Vincentx15 <vincent.mallet96@gmail.com>
2021-03-04 17:37:06 +0100

    commited setup

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#First install tf and keras
+conda install tensorflow-gpu=1.14
+conda install keras=2.2.4
+
+# Then add sklearn and avanti's own modules
+pip install scikit-learn
+pip install keras-genomics
+pip install seqdataloader
+pip install momma_dragonn
+
